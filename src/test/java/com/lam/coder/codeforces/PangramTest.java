fd75e214595edac72947ebb4dc81b46c3d6e3991
azuaje <ludovikoazuaje@yahoo.com>
2015-08-28 00:00:23 +0200

    CodeForces  solution.

---
+++ src/test/java/com/lam/coder/codeforces/PangramTest.java
@@ -0,0 +1,27 @@
+package com.lam.coder.codeforces;
+
+import org.junit.*;
+
+import com.lam.coder.codeforces.Pangram;
+
+@Ignore
+public class PangramTest {
+	Pangram pangram;
+	
+	@Before
+	public void setUp() throws Exception {
+		pangram = new Pangram();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		pangram  = null;
+	}
+
+	@Test
+	public void test() {
+		Assert.assertEquals("NO", pangram.find("toosmallword"));
+		Assert.assertEquals("YES", pangram.find("TheQuickBrownFoxJumpsOverTheLazyDog"));
+//		Assert.assertEquals("", pangram.find(""));
+	}
+}

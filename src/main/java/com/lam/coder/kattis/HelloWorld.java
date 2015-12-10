16db8378e9ac81ea314d8f0c5bea163286c95d75
azuaje <ludovikoazuaje@yahoo.com>
2015-12-11 00:00:24 +0100

    Kattis start.

---
+++ src/main/java/com/lam/coder/kattis/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lam.coder.kattis;
+
+/**
+ * Created by Usuario on 09/12/2015.
+ */
+public class HelloWorld {
+public static void main(String[] strings) {
+ System.out.println("Hello World!");
+}
+
+}

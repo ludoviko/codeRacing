5bef0efc315ae6ce51764fb8203ca75348a89b83
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-08-07 11:41:07 +0200

    Katti Main Utility.

--- src/main/java/com/lam/coder/kattis/dataLoad/CopyIntoMain.java
+++ src/main/java/com/lam/coder/kattis/dataLoad/CopyIntoMain.java
@@ -3,13 +3,13 @@ package com.lam.coder.kattis.dataLoad;
 import java.io.*;
 import java.util.Scanner;
 
-public class CopyIntoFile {
+public class CopyIntoMain {
 	public static void main(String[] args) throws IOException {
 		String dirSource = "./src/main/java/com/lam/coder/kattis/";
 		String dirOut = "./src/main/java/";
 
 		System.out
-		.println("Kattis: Utility for creating the a GivenClass.java Class -within the default package- from a given class.");
+		.println("Kattis: Utility for creating the Main.java Class -within the default package- from a given class.");
 
 		System.out
 		.println("Enter arg: the name of the source class to copy from, v.g. NextRound, you might use white spaces as they will be trimmed. ");

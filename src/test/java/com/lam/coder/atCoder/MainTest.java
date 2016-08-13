68b45bafaf7e144379a289d4a743a2d4e8c342c4
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-08-13 13:23:25 +0200

    At Coder jp: Main test.

---
+++ src/test/java/com/lam/coder/atCoder/MainTest.java
@@ -0,0 +1,31 @@
+package com.lam.coder.atCoder;
+
+import com.lam.coder.kattis.Main;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+	Main main;
+	
+	@Before
+	public void setUp() throws Exception {
+		main = new Main();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		main  = null;
+	}
+
+	@Test
+	public void test() {
+        Assert.assertThat("Should be the same.", main , CoreMatchers.is("expected"));
+//		Assert.assertEquals("", main.);
+//		Assert.assertArrayEquals(new int[] {}, main.);
+//		Assert.assertArrayEquals(new String[] {}, main.);
+	}
+}
+

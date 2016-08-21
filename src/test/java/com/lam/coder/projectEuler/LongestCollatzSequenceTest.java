38e68d8e8144cf0b14bd068137144cb6c5b1abf3
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-08-21 15:08:09 +0200

    Project Euler: LongestCollatzSequenceTest.

--- src/test/java/com/lam/coder/projectEuler/LongestCollatzSequenceTest.java
+++ src/test/java/com/lam/coder/projectEuler/LongestCollatzSequenceTest.java
@@ -1,6 +1,7 @@
 package com.lam.coder.projectEuler;
 
 
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,24 +11,23 @@ import com.lam.coder.projectEuler.LongestCollatzSequence;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-public class LongestCollatzSequenceTest extends TestCase {
+import static org.junit.Assert.assertThat;
+
+public class LongestCollatzSequenceTest  {
 	LongestCollatzSequence collatz; 
 	
 	@Before
-	protected void setUp() throws Exception {
-		super.setUp();
+	public void setUp() throws Exception {
 		collatz = new LongestCollatzSequence();
 	}
 
 	@After
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	public void tearDown() throws Exception {
 		collatz = null;
 	}
 
 	@Test
 	public void testFind() {
-		Assert.assertEquals(new Long(837799), collatz.find().get(0));
-	}
-
+        assertThat("Error: Should be the same.", collatz.find().get(0), CoreMatchers.is(837799L));
+    }
 }

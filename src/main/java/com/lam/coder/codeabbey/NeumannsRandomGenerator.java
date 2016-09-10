16944a6f7ea36521a2f304b1ad11f05d1202f781
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-09-10 22:25:21 +0200

    Code Abbey: Simple number generator.

---
+++ src/main/java/com/lam/coder/codeabbey/NeumannsRandomGenerator.java
@@ -0,0 +1,123 @@
+package com.lam.coder.codeabbey;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Code solution from: L.Azuaje.
+ */
+
+public class NeumannsRandomGenerator {
+    public static int find(int n) {
+        List<Integer> data = new LinkedList<Integer>();
+
+        while (!data.contains(n)) {
+            data.add(n);
+            n = n * n;
+            n = n / 100;
+            n = n % 10000;
+        }
+
+        return data.size();
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyScanner scanner = new MyScanner();
+        PrintStream out = System.out;
+
+        NeumannsRandomGenerator main = new NeumannsRandomGenerator();
+
+        int times  = scanner.nextInt();
+        int[] data = scanner.nextIntArray();
+
+        for (int i = 0; i < data.length ; i++) {
+            out.format("%d ",NeumannsRandomGenerator.find(data[i]));
+        }
+
+        scanner.reader.close();
+        out.close();
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader reader;
+
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public void close() throws IOException {
+            this.reader.close();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine().trim();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        String[] nextStringArray() {
+            String[] str = null;
+            try {
+                str = this.reader.readLine().trim().split(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextIntArray() {
+            String[] data = nextStringArray();
+            int[] a = new int[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        Integer[] nextIntegerArray() {
+            String[] data = nextStringArray();
+            Integer[] a = new Integer[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        long[] nextLongArray() {
+            String[] data = nextStringArray();
+            long[] a = new long[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Long.parseLong(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

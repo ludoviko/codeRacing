669105522089ec7de2c14623a7a44106f5c3da56
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-04 21:35:01 +0200

    Code info.

---
+++ src/main/java/com/lam/coder/topCoder/Main.java
@@ -0,0 +1,17 @@
+package com.lam.coder.topCoder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author 
+ *
+ *
+ *
+ */
+
+public class Main {
+}

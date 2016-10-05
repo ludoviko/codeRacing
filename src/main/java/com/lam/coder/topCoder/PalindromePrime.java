9b6a31a1d96267438a6963601101166c8f2e4368
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-05 19:23:18 +0200

    Palindrome Prime test.

---
+++ src/main/java/com/lam/coder/topCoder/PalindromePrime.java
@@ -0,0 +1,52 @@
+package com.lam.coder.topCoder;
+
+import java.util.Arrays;
+
+public class PalindromePrime {
+    public int count(int left, int right) {
+        boolean[] primesBool = find(right);
+
+        int count = 0;
+
+        for (int i = left; i <= right; i++) {
+            if (isPalindrome(i) && primesBool[i]  ) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static long reverse(long n, int base) {
+        long reversed = 0;
+        while (n > 0) {
+            reversed = base * reversed + n % base;
+            n /= base;
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(long n) {
+        return n == reverse(n, 10);
+    }
+
+    public static boolean[] find (int upTo) {
+        boolean[] primesBool = new boolean[upTo + 1];
+        Arrays.fill(primesBool, true);
+
+        primesBool[0] = false;
+        primesBool[1] = false;
+
+        int root = (int)Math.sqrt(upTo);
+
+        for (int i = 2; i <=root; i++) {
+            if (primesBool[i]) {
+                for (int j = i*i; j <=upTo; j+=i) {
+                    primesBool[j] = false;
+                }
+            }
+        }
+
+        return primesBool;
+    }
+}

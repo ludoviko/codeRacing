9244030a5c18bf135cd74ad4a493646e29fd0429
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-08 15:48:53 +0200

    Code forces: 580A 	Kefa And First Steps.

---
+++ src/main/java/com/lam/coder/codeforces/KefaAndFirstSteps.java
@@ -0,0 +1,124 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * @author Solution by L.Azuaje.
+ *
+ *   580A	Kefa And First Steps
+ */
+
+public class KefaAndFirstSteps {
+    public static int find( int[] data  ) {
+        int max = 1;
+        int seq = 1;
+
+        for (int i = 0; i < data.length - 1 ; i++) {
+            if ( data[i] <= data[i+1]  ) {
+                seq++;
+                if (seq > max) {
+                    max = seq;
+                }
+            } else {
+                seq = 1;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyScanner scanner = new MyScanner();
+        PrintStream out = System.out;
+
+        int n = scanner.nextInt();
+        int[] data = scanner.nextIntArray();
+
+        out.println(KefaAndFirstSteps.find(data));
+
+        scanner.close();
+        out.close();
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader reader;
+
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public void close() throws IOException {
+            this.reader.close();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine().trim();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        String[] nextStringArray() {
+            String[] str = null;
+            try {
+                str = this.reader.readLine().trim().split(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextIntArray() {
+            String[] data = nextStringArray();
+            int[] a = new int[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        Integer[] nextIntegerArray() {
+            String[] data = nextStringArray();
+            Integer[] a = new Integer[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        long[] nextLongArray() {
+            String[] data = nextStringArray();
+            long[] a = new long[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Long.parseLong(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

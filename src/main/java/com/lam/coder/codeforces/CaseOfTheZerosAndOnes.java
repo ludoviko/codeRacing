79fb92a8911692c5f9ad77e82385237b6f28f367
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-10 21:41:44 +0200

    Codeforces:  556A  Case Of The Zeros And Ones.

---
+++ src/main/java/com/lam/coder/codeforces/CaseOfTheZerosAndOnes.java
@@ -0,0 +1,127 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Solution by: L.Azuaje.
+ *
+ * Codeforces:  556A  Case Of The Zeros And Ones.
+ *
+ */
+
+public class CaseOfTheZerosAndOnes {
+
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		CaseOfTheZerosAndOnes main = new CaseOfTheZerosAndOnes();
+		
+		int n = scanner.nextInt();
+		String string = scanner.next();
+        StringBuilder solution = new StringBuilder();
+
+        char[] chars = string.toCharArray();
+
+        int ones = 0;
+        int zeroes = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '0') {
+                zeroes++;
+            } else {
+                ones++;
+            }
+        }
+
+        int dif = Math.abs(ones - zeroes);
+
+		out.println(dif);
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

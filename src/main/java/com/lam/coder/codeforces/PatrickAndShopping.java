ff82e2b21ddd293024733c69d46432a4e37841cd
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-11 11:59:34 +0200

    Code Forces: PatrickAndShopping.

---
+++ src/main/java/com/lam/coder/codeforces/PatrickAndShopping.java
@@ -0,0 +1,122 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Solution by: L.Azuaje.
+ *
+ * Codeforces: 599A	    Patrick And Shopping .
+ *
+ */
+
+public class PatrickAndShopping {
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		int[] data  = scanner.nextIntArray();
+        int ab = data[0];
+        int bc = data[1];
+        int ac = data[2];
+
+        int d = 0;
+
+        if (ab <= bc) {
+            d = ab;
+            d += Math.min(ab + bc, ac);
+            d += Math.min(bc, ac + ab);
+        } else {
+            d = bc;
+            d += Math.min(ab + bc, ac);
+            d += Math.min(ab, ac + bc);
+        }
+
+		out.println(d);
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

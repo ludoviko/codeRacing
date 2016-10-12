7d5acaff230113759000c8c79e37b4d764586994
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-12 19:13:13 +0200

    Codeforces: 707A	Brain's Photos.

---
+++ src/main/java/com/lam/coder/codeforces/BrainsPhotos.java
@@ -0,0 +1,126 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Solution by: L.Azuaje.
+ *
+ * Codeforces: 707A	Brain's Photos.
+ *
+ */
+
+public class BrainsPhotos {
+	
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		BrainsPhotos main = new BrainsPhotos();
+		
+		int[] info  = scanner.nextIntArray();
+
+        String string;
+        boolean isColor = false;
+
+        for (int i = 0; i < info[0]; i++) {
+            string = scanner.next();
+
+            if (string.contains("C") || string.contains("M") || string.contains("Y") ) {
+                isColor = true;
+                break;
+            }
+        }
+
+        if (isColor) {
+            out.println("#Color");
+        } else {
+            out.println("#Black&White");
+        }
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

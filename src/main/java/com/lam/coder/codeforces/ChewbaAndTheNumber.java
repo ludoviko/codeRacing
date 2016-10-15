53b79506b92eaead1dec7307d90a532de4a0c1a5
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-15 21:59:40 +0200

    Codeforces: 514A.  Round #291 (Div. 2). 	Chewbaсca and Number

--- src/main/java/com/lam/coder/codeforces/ChewbaAndTheNumber.java
+++ src/main/java/com/lam/coder/codeforces/ChewbaAndTheNumber.java
@@ -6,78 +6,92 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+/**
+ * @author Solution by: L.Azuaje.
+ *         <p>
+ *         Codeforces: 514A.  Round #291 (Div. 2). 	Chewbaсca and Number.
+ */
+
+
 public class ChewbaAndTheNumber {
-	
-	public String find(String string) {
-		char[] data = string.toCharArray();
-		char min  = data[0];
-		
-		for (int i = 0; i < data.length; i++) {
-			if ( data[i] == '0' ) {
-				continue;
-			}
-			min = data[i] < min ? data[i] : min;
-		}
 
-		for (int i = 0; i < data.length; i++) {
-			if ( data[i] == '0' ) {
-				continue;
-			}
-			data[i] = data[i] > min ? min : data[i];
-		}
-		
-		
-		return new String(data);
-	}
-	
-	public static void main(String[] args) throws IOException {
-		MyScanner scanner = new MyScanner();
-		PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    public static String find(String string) {
+        char[] data = string.toCharArray();
+        char nine = '9';
+        String solution = "";
 
-		ChewbaAndTheNumber main = new ChewbaAndTheNumber();
-		
-		String string = scanner.next();
+        if (string.matches("$9+^")) {
+            for (int i = 0; i < data.length; i++) {
+                if (nine - data[i] < data[i] - '0' && nine - data[i] != 0) {
+                    solution += (nine - data[i]);
+                } else {
+                    solution += data[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < data.length; i++) {
+                if (nine - data[i] < data[i] - '0') {
+                    solution += (nine - data[i]);
+                } else {
+                    solution += data[i];
+                }
+            }
+        }
 
-		out.println(main.find(string));
+        if (solution.charAt(0) == '0') {
+            solution = "9" + solution.substring(1);
+        }
+        return solution;
+    }
 
-		scanner.reader.close();
-		out.close();
-	}
+    public static void main(String[] args) throws IOException {
+        MyScanner scanner = new MyScanner();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
 
-	// -----------MyScanner class for faster input----------
-	public static class MyScanner {
-		BufferedReader reader;
+        ChewbaAndTheNumber main = new ChewbaAndTheNumber();
 
-		public MyScanner() {
-			this.reader = new BufferedReader(new InputStreamReader(System.in));
-		}
+        String string = scanner.next();
 
-		public void close() throws IOException {
-			this.reader.close();
-		}
+        out.println(main.find(string));
 
-		int nextInt() {
-			return Integer.parseInt(this.next());
-		}
+        scanner.reader.close();
+        out.close();
+    }
 
-		long nextLong() {
-			return Long.parseLong(this.next());
-		}
+    // -----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader reader;
 
-		double nextDouble() {
-			return Double.parseDouble(this.next());
-		}
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() {
-			String str = "";
-			try {
-				str = this.reader.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
+        public void close() throws IOException {
+            this.reader.close();
+        }
 
-	}
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
 }
 

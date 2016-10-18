fbd3a75c7820df6295525dc1b38f77fa896d124b
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-18 17:50:58 +0200

    Problem:     Wet Shark and Odd and Even.  Codeforces:  621A   #341 Div-2.

---
+++ src/main/java/com/lam/coder/codeforces/WetSharkAndOddAndEven.java
@@ -0,0 +1,134 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 
+ * @author Code Forces.  Solution by: L.Azuaje.
+ *
+ * Problem:     Wet Shark and Odd and Even.
+ *
+ * Codeforces:  621A   #341 Div-2.
+ *
+ */
+
+public class WetSharkAndOddAndEven {
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		int n = scanner.nextInt();
+        int[] data = scanner.nextIntArray();
+        List<Integer> odds = new LinkedList<Integer>();
+        List<Integer> evens = new LinkedList<Integer>();
+        long sumOdds = 0;
+        long sumEvens = 0;
+
+        for (int d : data) {
+            if (d % 2 == 0) {
+                sumEvens += d;
+                evens.add(d);
+            } else {
+                sumOdds += d;
+                odds.add(d);
+            }
+        }
+
+        Collections.sort(odds);
+
+        if ( sumOdds % 2 != 0 ) {
+            sumOdds -= odds.get(0);
+        }
+
+        out.println(sumOdds + sumEvens);
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

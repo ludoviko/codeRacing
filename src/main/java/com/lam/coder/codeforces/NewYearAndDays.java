f5989d286ac5bef6c77085dbe1727e49e3cdd43f
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-11-01 01:47:18 +0100

    Code Forces:    611A	  Good Bye 2015    New Year and Days .

---
+++ src/main/java/com/lam/coder/codeforces/NewYearAndDays.java
@@ -0,0 +1,126 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Code Forces.  Solution by: L.Azuaje.
+ *
+ * Problem:  611A	New Year and Days.
+ *
+ * Used at Codeforces: Good Bye 2015  .
+ *
+ */
+
+public class NewYearAndDays {
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		NewYearAndDays main = new NewYearAndDays();
+		
+        String string = scanner.next();
+        String[] data = string.split(" ");
+
+        if (data[2].contains("week")) {
+            if (data[0].equals("5") || data[0].equals("6")) {
+                out.println(53);
+            } else {
+                out.println(52);
+            }
+        } else {
+            if (data[0].equals("30")) {
+                out.println(11);
+            } else if (data[0].equals("31")) {
+                out.println(7);
+            } else {
+                out.println(12);
+            }
+        }
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

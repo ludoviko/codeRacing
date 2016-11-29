62efe9e4e322705e2a736359770d9cc01d5b0a9c
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-11-29 22:57:09 +0100

    Acmp: Test template.

---
+++ src/test/java/com/lam/coder/acmp/MainTest.java
@@ -0,0 +1,29 @@
+package com.lam.coder.acmp;
+
+import com.lam.coder.codeabbey.Main;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+	Main main;
+	
+	@Before
+	public void setUp() throws Exception {
+		main = new Main();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		main  = null;
+	}
+
+	@Test
+	public void test() {
+//        Assert.assertThat("Should be the same.", main , CoreMatchers.is("expected"));
+//		Assert.assertEquals("", main.);
+//		Assert.assertArrayEquals(new int[] {}, main.);
+//		Assert.assertArrayEquals(new String[] {}, main.);
+	}
+}
+

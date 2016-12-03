e23da40560a20dcb0cf49caf9ca372a6cf6de334
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-12-03 12:23:00 +0100

    Code Forces: 734A	 #379 Div-2               Anton and Danik.

---
+++ src/main/java/com/lam/coder/codeforces/AntonAndDanik.java
@@ -0,0 +1,123 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Code Forces.  Solution by: L.Azuaje.
+ *
+ * Problem: 734A  Anton and Danik.
+ *
+ * Used at Codeforces: #379 Div-2.
+ *
+ */
+
+public class AntonAndDanik {
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		AntonAndDanik main = new AntonAndDanik();
+		
+		int n = scanner.nextInt();
+        String string = scanner.next();
+
+        int r = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (string.charAt(i) == 'A' ) {
+                r++;
+            } else {
+                r--;
+            }
+        }
+
+        String result = r > 0 ? "Anton" : r < 0 ? "Danik" : "Friendship";
+        out.println(result);
+
+        scanner.close();
+        out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

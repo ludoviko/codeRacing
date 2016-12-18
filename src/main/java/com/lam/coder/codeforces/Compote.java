9da13353b5f9fe2a6cce0e199bcd122648504c32
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-12-18 15:22:10 +0100

    Code Forces: 746A Compot.

---
+++ src/main/java/com/lam/coder/codeforces/Compote.java
@@ -0,0 +1,131 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Code Forces.  Solution by: L.Azuaje.
+ *
+ * Problem: 746A Compot.
+ *
+ * Used at Codeforces: #386 Div-2.
+ *
+ */
+
+public class Compote {
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+        // 1:2:4
+		int lemons  = scanner.nextInt();
+        int apples  = scanner.nextInt();
+        int pears  = scanner.nextInt();
+        int summa = 0;
+
+        for (int i = lemons; i >= 0 ; i--) {
+            summa += i;
+            if (i * 2 <= apples) {
+                summa += i * 2;
+            } else {
+                summa = 0;
+                continue;
+            }
+
+            if (i * 4 <= pears) {
+                summa += i * 4;
+            } else {
+                summa = 0;
+                continue;
+            }
+            break;
+        }
+
+		out.println(summa);
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

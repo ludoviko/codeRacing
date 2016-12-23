4e4b90bb2dac969ad068f867521658c54dd9414c
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-12-23 23:51:41 +0100

    Code Forces: 731A	  #376 Div-2              Night at the Museum .

---
+++ src/main/java/com/lam/coder/codeforces/NightAtTheMuseum.java
@@ -0,0 +1,119 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Code Forces.  Solution by: L.Azuaje.
+ *
+ * Problem: 731A  Night at the Museum.
+ *
+ * Used at Codeforces:  #376 Div-2.
+ *
+ */
+
+public class NightAtTheMuseum {
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+        int alphabit_len = 26;
+        int s = 0;
+
+        String string = scanner.next();
+        string  = 'a' + string;
+
+        for (int i = 1; i < string.length(); i++) {
+            s += Math.min( Math.abs( string.charAt(i-1) - string.charAt(i)),
+                    alphabit_len - Math.abs( string.charAt(i-1) - string.charAt(i)
+                    ));
+        }
+
+		out.println(s);
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

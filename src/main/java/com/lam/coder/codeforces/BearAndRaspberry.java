f3e2d9ded7798b7ae2003c2beb67d94c929641b4
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-12-28 00:05:22 +0100

    Code Forces: 385A  #226 Div-2	              Bear and Raspberry .

---
+++ src/main/java/com/lam/coder/codeforces/BearAndRaspberry.java
@@ -0,0 +1,121 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author Code Forces.  Solution by: L.Azuaje.
+ *
+ * Problem: 385A   Bear and Raspberry .
+ *
+ * Used at Codeforces: #226 Div-2.
+ *
+ */
+
+public class BearAndRaspberry {
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+        int[] data = scanner.nextIntArray();
+        int[] data2 = scanner.nextIntArray();
+        int max = 0;
+
+        for (int i = 0; i < data2.length - 1 ; i++) {
+            if (data2[i] - data2[i+1] > max) {
+                max = data2[i] - data2[i+1];
+            }
+        }
+
+        if ( max - data[1] > 0) {
+            out.println(max - data[1] );
+        } else {
+            out.println(0);
+        }
+
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+	}
+}
+

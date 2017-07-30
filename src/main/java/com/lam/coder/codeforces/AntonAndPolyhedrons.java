a746bc35c71fe9ff4d3e5cc72a34793c49bd4df6
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-07-30 12:38:43 +0200

    Code Forces: 785A  Anton and Polyhedrons.

---
+++ src/main/java/com/lam/coder/codeforces/AntonAndPolyhedrons.java
@@ -0,0 +1,273 @@
+package com.lam.coder.codeforces;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+
+/**
+ * 
+ * @author Code Forces.  Solution by: L.Azuaje.
+ *
+ * Problem: .
+ *
+ * Used at Codeforces: .
+ *
+ */
+
+public class AntonAndPolyhedrons {
+    public static final String Tetrahedron = "Tetrahedron";
+    public static final String Cube = "Cube";
+    public static final String Octahedron = "Octahedron";
+    public static final String Dodecahedron = "Dodecahedron";
+    public static final String Icosahedron = "Icosahedron";
+
+    private Map<String, Integer> map;
+
+    AntonAndPolyhedrons() {
+        map = new HashMap<String, Integer>();
+        map.put(Tetrahedron, 4);
+        map.put(Cube, 6);
+        map.put(Octahedron, 8);
+        map.put(Dodecahedron, 12);
+        map.put(Icosahedron, 20);
+    }
+
+
+    public  Map<String, Integer> getMap() {
+        return this.map;
+    }
+
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		AntonAndPolyhedrons main = new AntonAndPolyhedrons();
+		
+		int n = scanner.nextInt();
+		int s = 0;
+
+        for (int i = 0; i < n; i++) {
+            s += main.getMap().get(scanner.next());
+        }
+
+		out.println(s);
+		scanner.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+    public static class FastInputReader {
+        private byte[] buf = new byte[16384];
+        private int    curChar;
+        private int    numChars;
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = System.in.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+    }
+
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+        float nextFloat() {
+            return Float.parseFloat(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+
+        float[] nextFloatArray() {
+            String[] data  = nextStringArray();
+            float[] a  = new float[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Float.parseFloat(data[i]);
+            }
+
+            return a;
+        }
+
+        double[] nextDoubleArray() {
+            String[] data  = nextStringArray();
+            double[] a  = new double[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Double.parseDouble(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

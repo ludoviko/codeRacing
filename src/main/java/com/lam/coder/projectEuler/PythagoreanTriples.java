3536a7922f05196ea0fcf36df8d43868455d4850
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-09-06 00:21:58 +0200

    Project Euler: update.

---
+++ src/main/java/com/lam/coder/projectEuler/PythagoreanTriples.java
@@ -0,0 +1,205 @@
+package com.lam.coder.projectEuler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * @author L.Azuaje.
+ *         <p>
+ *         <p>
+ *         Problem 9: Special Pythagorean triplet
+ *         <p>
+ *         A Pythagorean triplet is a set of three natural numbers, a < b < c,
+ *         for which, a^2 + b^2 = c^2
+ *         <p>
+ *         For example, 32 + 42 = 9 + 16 = 25 = 52.
+ *         <p>
+ *         There exists exactly one Pythagorean triplet for which a + b + c =
+ *         1000. Find the product abc.
+ *         <p>
+ *         a = 2mn
+ *         b = m*m - n*n
+ *         c = m*m + n*n
+ *         <p>
+ *         Answer = 31875000.
+ *         <p>
+ *         <p>
+ *         a*a + b*b = c*c;
+ *         a + b + c = n;
+ *         <p>
+ *         <p>
+ *         Solution:
+ *         <p>
+ *         b = (n . n - 2an)/2(n-a);
+ */
+
+public class PythagoreanTriples {
+    private long a;
+    private long b;
+    private long c;
+    private long summa;
+
+    public PythagoreanTriples(long summa) {
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        // summa = a + b + c.
+        this.summa = summa;
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyScanner scanner = new MyScanner();
+        PrintStream out = System.out;
+
+        PythagoreanTriples main;
+
+        // Number of test cases.
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            main = new PythagoreanTriples(scanner.nextInt());
+            main.find();
+            if (main.getC() == -1) {
+                out.format("%d ", main.getC());
+            } else {
+                out.format("%d ", main.getA() * main.getB() * main.getC());
+            }
+        }
+
+        scanner.reader.close();
+        out.close();
+    }
+
+    public long getC() {
+        return this.c;
+    }
+
+    public long getA() {
+        return this.a;
+    }
+
+    public long getB() {
+        return this.b;
+    }
+
+    public void find() {
+        while (++a < summa) {
+            if ((summa * summa - 2 * a * summa) % (2 * summa - 2 * a) == 0) {
+                b = (long) ((summa * summa - 2 * a * summa) / (2 * summa - 2 * a));
+                break;
+            }
+        }
+
+        if (b == 0) {
+            // no solution.
+            this.a = -1;
+            this.b = -1;
+            this.c = -1;
+        } else {
+            this.c = (long) Math.sqrt(a * a + b * b);
+        }
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader reader;
+
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public void close() throws IOException {
+            this.reader.close();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine().trim();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        String[] nextStringArray() {
+            String[] str = null;
+            try {
+                str = this.reader.readLine().trim().split(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextIntArray() {
+            String[] data = nextStringArray();
+            int[] a = new int[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        Integer[] nextIntegerArray() {
+            String[] data = nextStringArray();
+            Integer[] a = new Integer[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        long[] nextLongArray() {
+            String[] data = nextStringArray();
+            long[] a = new long[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Long.parseLong(data[i]);
+            }
+
+            return a;
+        }
+
+        float[] nextFloatArray() {
+            String[] data = nextStringArray();
+            float[] a = new float[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Float.parseFloat(data[i]);
+            }
+
+            return a;
+        }
+
+        double[] nextDoubleArray() {
+            String[] data = nextStringArray();
+            double[] a = new double[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Double.parseDouble(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

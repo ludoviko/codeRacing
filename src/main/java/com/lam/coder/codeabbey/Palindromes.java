2681e253a60d6d45712374dea65e2df6fa1c7091
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-10-10 19:57:10 +0200

    Code Abbey:  Palindromes.

---
+++ src/main/java/com/lam/coder/codeabbey/Palindromes.java
@@ -0,0 +1,146 @@
+package com.lam.coder.codeabbey;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * @author L.Azuaje.
+ */
+
+public class Palindromes {
+    private String string;
+    private ReverseString reverseString;
+
+    public Palindromes(String string) {
+        this.string = string.toLowerCase().replaceAll("[^a-z]", "");
+        reverseString = new ReverseString(this.string);
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyScanner scanner = new MyScanner();
+        PrintStream out = System.out;
+        Palindromes main = null;
+
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            String string = scanner.next();
+            main = new Palindromes(string);
+            if (main.isPalindrome()) {
+                out.format("%s ", "Y");
+            } else {
+                out.format("%s ", "N");
+            }
+        }
+
+        scanner.reader.close();
+        out.close();
+    }
+
+    public boolean isPalindrome() {
+        reverseString.reverse();
+        return this.string.equals(reverseString.getReverse());
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader reader;
+
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public void close() throws IOException {
+            this.reader.close();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine().trim();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        String[] nextStringArray() {
+            String[] str = null;
+            try {
+                str = this.reader.readLine().trim().split(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextIntArray() {
+            String[] data = nextStringArray();
+            int[] a = new int[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        Integer[] nextIntegerArray() {
+            String[] data = nextStringArray();
+            Integer[] a = new Integer[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        long[] nextLongArray() {
+            String[] data = nextStringArray();
+            long[] a = new long[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Long.parseLong(data[i]);
+            }
+
+            return a;
+        }
+
+        float[] nextFloatArray() {
+            String[] data = nextStringArray();
+            float[] a = new float[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Float.parseFloat(data[i]);
+            }
+
+            return a;
+        }
+
+        double[] nextDoubleArray() {
+            String[] data = nextStringArray();
+            double[] a = new double[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Double.parseDouble(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

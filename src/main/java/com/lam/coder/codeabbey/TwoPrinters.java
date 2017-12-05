69d655f00badf0f491486aaa23caa467dee5e2ee
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-12-06 00:28:16 +0100

    Code Abbey: 22	 Two Printers  .

---
+++ src/main/java/com/lam/coder/codeabbey/TwoPrinters.java
@@ -0,0 +1,151 @@
+package com.lam.coder.codeabbey;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Locale;
+
+/**
+ * @author L.Azuaje.
+ *         <p>
+ *         Code Abbey: 22	 Two Printers.
+ */
+
+public class TwoPrinters {
+    public TwoPrinters() {
+    }
+
+    public static int findFrom(int a, int b, int c) {
+        int solution = -1;
+        int k = (int) (c / (1.0 / a + 1.0 / b));
+
+        for (int i = k; i < 2 * k; i++) {
+            if (i / a + i / b >= c) {
+                solution = i;
+                break;
+            }
+        }
+
+        return solution;
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyScanner scanner = new MyScanner();
+        PrintStream out = System.out;
+
+
+        int n = scanner.nextInt();
+        int[] data = null;
+        for (int i = 0; i < n; i++) {
+            data = scanner.nextIntArray();
+            out.format(Locale.US, "%d ", findFrom(data[0], data[1], data[2]));
+        }
+
+        out.println();
+
+        scanner.reader.close();
+        out.close();
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader reader;
+
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public void close() throws IOException {
+            this.reader.close();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine().trim();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        String[] nextStringArray() {
+            String[] str = null;
+            try {
+                str = this.reader.readLine().trim().split(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextIntArray() {
+            String[] data = nextStringArray();
+            int[] a = new int[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        Integer[] nextIntegerArray() {
+            String[] data = nextStringArray();
+            Integer[] a = new Integer[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        long[] nextLongArray() {
+            String[] data = nextStringArray();
+            long[] a = new long[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Long.parseLong(data[i]);
+            }
+
+            return a;
+        }
+
+        float[] nextFloatArray() {
+            String[] data = nextStringArray();
+            float[] a = new float[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Float.parseFloat(data[i]);
+            }
+
+            return a;
+        }
+
+        double[] nextDoubleArray() {
+            String[] data = nextStringArray();
+            double[] a = new double[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Double.parseDouble(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

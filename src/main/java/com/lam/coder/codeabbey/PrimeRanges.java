526914137f092cce85438b4290f387f7ebd8023b
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-12-17 21:21:44 +0100

    Code Abbey: 62	 Prime Ranges                 Number Theory  .

---
+++ src/main/java/com/lam/coder/codeabbey/PrimeRanges.java
@@ -0,0 +1,153 @@
+package com.lam.coder.codeabbey;
+
+import com.lam.mathematics.Primes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+/**
+ * @author L.Azuaje.
+ *         <p>
+ *         Code Abbey: 62	 Prime Ranges                 Number Theory  .
+ */
+
+public class PrimeRanges {
+
+    public PrimeRanges() {
+    }
+
+//	public find() {
+//	}
+
+    public static void main(String[] args) throws IOException {
+        MyScanner scanner = new MyScanner();
+        PrintStream out = System.out;
+
+        PrimeRanges main = new PrimeRanges();
+
+        Primes calculate = new Primes();
+        calculate.find(3000000);
+        long[] array = calculate.getArray();
+
+        int n = scanner.nextInt();
+        int[] data = null;
+        int index1 = -1;
+        int index2 = -1;
+
+        for (int i = 0; i < n; i++) {
+            data = scanner.nextIntArray();
+            index1 = Arrays.binarySearch(array, data[0]);
+            index2 = Arrays.binarySearch(array, data[1]);
+            out.format("%d ", index2 - index1 + 1);
+        }
+
+        out.println();
+
+        scanner.reader.close();
+        out.close();
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader reader;
+
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public void close() throws IOException {
+            this.reader.close();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine().trim();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        String[] nextStringArray() {
+            String[] str = null;
+            try {
+                str = this.reader.readLine().trim().split(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextIntArray() {
+            String[] data = nextStringArray();
+            int[] a = new int[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        Integer[] nextIntegerArray() {
+            String[] data = nextStringArray();
+            Integer[] a = new Integer[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        long[] nextLongArray() {
+            String[] data = nextStringArray();
+            long[] a = new long[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Long.parseLong(data[i]);
+            }
+
+            return a;
+        }
+
+        float[] nextFloatArray() {
+            String[] data = nextStringArray();
+            float[] a = new float[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Float.parseFloat(data[i]);
+            }
+
+            return a;
+        }
+
+        double[] nextDoubleArray() {
+            String[] data = nextStringArray();
+            double[] a = new double[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Double.parseDouble(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

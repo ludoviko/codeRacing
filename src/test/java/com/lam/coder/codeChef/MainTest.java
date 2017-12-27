4fdeed677e5544aeadcb759b608b44169d5f4487
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-12-27 21:37:55 +0100

    Code Chef: Tests.

---
+++ src/test/java/com/lam/coder/codeChef/MainTest.java
@@ -0,0 +1,31 @@
+package com.lam.coder.codeChef;
+
+
+import com.lam.coder.codeforces.Main;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+    com.lam.coder.codeforces.Main main;
+
+    @Before
+    public void setUp() throws Exception {
+        main = new Main();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        main = null;
+    }
+
+    @Test
+    public void test() {
+//        Assert.assertThat("Should be the same.", main, CoreMatchers.is("expected"));
+//		Assert.assertEquals("", main.);
+//		Assert.assertArrayEquals(new int[] {}, main.);
+//		Assert.assertArrayEquals(new String[] {}, main.);
+//        Assert.assertThat("string", CoreMatchers.instanceOf(String.class) );
+    }
+}
+

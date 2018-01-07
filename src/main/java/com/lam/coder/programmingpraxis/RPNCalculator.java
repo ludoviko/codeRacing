416f136af7ce6f8411ad0fad3d392918fd494ff0
L.Azuaje <ludovikoazuaje@yahoo.com>
2018-01-07 15:53:41 +0100

    programmingpraxis: update.

--- src/main/java/com/lam/coder/programmingpraxis/RPNCalculator.java
+++ src/main/java/com/lam/coder/programmingpraxis/RPNCalculator.java
@@ -1,12 +1,12 @@
 package com.lam.coder.programmingpraxis;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Stack;
 
 /**
- * 
- * @author Ludoviko Azuaje
+ *
+ * @author programmingpraxis.com
+ *
+ * Solution by: L.Azuaje.
  * 
  *         RPN Calculator February 19, 2009
  * 
@@ -20,18 +20,26 @@ import java.util.Stack;
  */
 
 public class RPNCalculator {
-	public float evaluate(String expression) throws Exception {
-		String[] data = expression.split(" ");
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+    }
 
-		Stack<String> stack = new Stack<String>();
-		float op1, op2, op3 = 0;
+    // XXX Problems with scale and rounding mode
+// 	public BigDecimal evaluateBD(String expression) throws Exception {
+
+    public double evaluate(String expression) throws Exception {
+        String[] data = expression.split(" ");
+
+        Stack<Double> stack = new Stack<Double>();
+        double op1, op2, op3 = -1;
 
 		for (int i = 0; i < data.length; i++) {
-
 			if (data[i].matches("\\+|\\*|\\/|\\-")) {
-				op2 = Float.parseFloat(stack.pop());
-				op1 = Float.parseFloat(stack.pop());
-				if (data[i].equals("+")) {
+                op2 = stack.pop();
+                op1 = stack.pop();
+                if (data[i].equals("+")) {
 					op3 = op1 + op2;
 				} else if (data[i].equals("-")) {
 					op3 = op1 - op2;
@@ -39,75 +47,23 @@ public class RPNCalculator {
 					op3 = op1 * op2;
 				} else if (data[i].equals("/")) {
 					op3 = op1 / op2;
-				}
-				stack.push(op3 + "");
+                }
+                stack.push(op3);
+            } else if (data[i].equals("~")) {
+                // Unary operation: negation.
+                op1 = stack.pop();
+                op1 = op1 * (-1);
+                stack.push(op1);
+            } else {
+                stack.push(Double.parseDouble(data[i]));
+            }
+        }
 
-			} else if (data[i].equals("~")) {
-				op1 = Float.parseFloat(stack.pop());
-				op1 = op1 * (-1);
-				stack.push(op1 + "");
-			} else {
-				stack.push(data[i]);
-			}
-		}
+        if (stack.size() > 1) {
+            throw new Exception("Data malformed");
+        }
 
-		if (stack.size() == 2) {
-			throw new Exception("Data malformed");
-		}
-
-		return Float.parseFloat(stack.pop());
-	}
-
-	// XXX Problems with scale and rounding mode 
-	public BigDecimal evaluateBD(String expression) throws Exception {
-		String[] data = expression.split(" ");
-
-		Stack<String> stack = new Stack<String>();
-		BigDecimal op1 = null, op2 = null, op3 = null;
-
-		for (int i = 0; i < data.length; i++) {
-
-			if (data[i].matches("\\+|\\*|\\/|\\-")) {
-				op2 = new BigDecimal(stack.pop());
-				op1 = new BigDecimal(stack.pop());
-				if (data[i].equals("+")) {
-					op3 = op1.add(op2);
-				} else if (data[i].equals("-")) {
-					op3 = op1.subtract(op2);
-				} else if (data[i].equals("*")) {
-					op3 = op1.multiply(op2);
-				} else if (data[i].equals("/")) {
-					op3 = op1.divide(op2, 4,  RoundingMode.CEILING);
-				}
-
-				stack.push(op3.toString());
-
-			} else if (data[i].equals("~")) {
-				op2 = new BigDecimal(stack.pop());
-				op2 = op2.multiply(new BigDecimal(-1));
-				stack.push(op2.toString());
-			} else {
-				stack.push(data[i]);
-			}
-		}
-
-		if (stack.size() == 2) {
-			throw new Exception("Data malformed");
-		}
-
-		return new BigDecimal(stack.pop());
-	}
-
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("*".matches("\\+|\\*|\\/|\\-"));
-		System.out.println("/".matches("\\+|\\*|\\/|\\-"));
-		System.out.println("+".matches("\\+|\\*|\\/|\\-"));
-		System.out.println("-".matches("\\+|\\*|\\/|\\-"));
-
-	}
+        return stack.pop();
+    }
 
 }

e2c4995b7b238aaac5a9ece1e78c865a4453eefc
L.Azuaje <ludovikoazuaje@yahoo.com>
2018-04-08 00:44:33 +0200

    Code Forces:  Problem: 276B  #169 Div-2   Little Girl and Game.

---
+++ src/main/java/com/lam/coder/codeforces/LittleGirlAndGame.java
@@ -0,0 +1,277 @@
+package com.lam.coder.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.InputMismatchException;
+
+/**
+ * @author Code Forces.
+ *         <p>
+ *         Solution by: L.Azuaje.
+ *         <p>
+ *         Problem: 276B  #169 Div-2   Little Girl and Game.
+ */
+
+public class LittleGirlAndGame {
+    private String string;
+
+    public LittleGirlAndGame(String string) {
+        this.string = string;
+    }
+
+    public int countOdds() {
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        char[] alphabet = letters.toCharArray();
+        int[] frequency = new int[alphabet.length];
+
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLetter(string.charAt(i))) {
+                frequency[string.charAt(i) - 'a']++;
+            }
+        }
+
+        // Count odds.
+        int odds = 0;
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] % 2 == 1) {
+                odds++;
+            }
+        }
+
+        return odds;
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastInputReader scanner = new FastInputReader();
+        PrintStream out = System.out;
+
+        String string = scanner.nextString();
+
+        LittleGirlAndGame game = new LittleGirlAndGame(string);
+        int odds = game.countOdds();
+
+        if (odds % 2 == 1 || odds == 0) {
+            out.println("First");
+        } else {
+            out.println("Second");
+        }
+        out.close();
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class FastInputReader {
+        private byte[] buf = new byte[16384];
+        private int curChar;
+        private int numChars;
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = System.in.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+    }
+
+    public static class MyScanner {
+        BufferedReader reader;
+
+        public MyScanner() {
+            this.reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public void close() throws IOException {
+            this.reader.close();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(this.next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(this.next());
+        }
+
+        float nextFloat() {
+            return Float.parseFloat(this.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(this.next());
+        }
+
+        String next() {
+            String str = "";
+            try {
+                str = this.reader.readLine().trim();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        String[] nextStringArray() {
+            String[] str = null;
+            try {
+                str = this.reader.readLine().trim().split(" ");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextIntArray() {
+            String[] data = nextStringArray();
+            int[] a = new int[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        Integer[] nextIntegerArray() {
+            String[] data = nextStringArray();
+            Integer[] a = new Integer[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(data[i]);
+            }
+
+            return a;
+        }
+
+        long[] nextLongArray() {
+            String[] data = nextStringArray();
+            long[] a = new long[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Long.parseLong(data[i]);
+            }
+
+            return a;
+        }
+
+        float[] nextFloatArray() {
+            String[] data = nextStringArray();
+            float[] a = new float[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Float.parseFloat(data[i]);
+            }
+
+            return a;
+        }
+
+        double[] nextDoubleArray() {
+            String[] data = nextStringArray();
+            double[] a = new double[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Double.parseDouble(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+

c5e1ca24480a4cdb2a80efbed0aa9e5e7e3ca316
ludoviko <ludovikoazuaje@yahoo.com>
2018-12-24 10:46:47 +0100

    Hacker Earth: Anagrams.

---
+++ src/main/java/com/lam/coder/hackerearth/Anagrams.java
@@ -0,0 +1,187 @@
+package com.lam.coder.hackerearth;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+
+/**
+ * @author /www.hackerearth.com.
+ * <p>
+ * Solution by: L.Azuaje.
+ * <p>
+ * Problem:
+ * <p>
+ * Given two strings, a and b , that may or may not be of the same length,
+ * determine the minimum number of character deletions required to make a and b anagrams.
+ * Any characters can be deleted from either of the strings.
+ * <p>
+ * Input :
+ * <p>
+ * test cases,t
+ * two strings a and b, for each test case
+ * Output:
+ * Number of deletions.
+ */
+
+public class Anagrams {
+    public static final String YES = "YES";
+    public static final String NO = "NO";
+
+    private static final String LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    private static final char[] LETTER_CHARS = LETTERS.toCharArray();
+
+    public static final Map<Character, Integer> countData(String string) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            map.put(new Character(string.charAt(i)), map.getOrDefault(new Character(string.charAt(i)), 0) + 1);
+        }
+
+        return map;
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastInputReader scanner = new FastInputReader();
+        PrintStream out = System.out;
+
+        int cases = scanner.nextInt();
+
+        for (int i = 0; i < cases; i++) {
+            int differnce = 0;
+            String string = scanner.nextString();
+            String string2 = scanner.nextString();
+
+            Map<Character, Integer> map = countData(string);
+            Map<Character, Integer> map2 = countData(string2);
+
+            for (Character c : LETTER_CHARS) {
+                differnce += Math.abs(map.getOrDefault(c, 0) - map2.getOrDefault(c, 0));
+            }
+
+            out.println(differnce);
+        }
+        out.close();
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class FastInputReader {
+        private byte[] buf = new byte[16384];
+        private int curChar;
+        private int numChars;
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = System.in.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public Integer[] nextIntegerArray(int n) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+    }
+}
+

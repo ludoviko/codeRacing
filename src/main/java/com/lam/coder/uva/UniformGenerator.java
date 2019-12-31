eb7bda7c67c619fc2ef6da9afdfb1c4e4398b113
ludoviko <ludovikoazuaje@yahoo.com>
2019-12-31 15:34:14 +0100

    UVA Vol/Cod/Name: 4  408  Uniform Generator.

---
+++ src/main/java/com/lam/coder/uva/UniformGenerator.java
@@ -0,0 +1,182 @@
+package com.lam.coder.uva;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Set;
+
+
+/**
+ *
+ * @author UVA on Line.
+ *
+ * Solution by L.Azuaje.
+ *
+ *  UVA Vol/Cod/Name: 4  408  Uniform Generator.
+ *
+ */
+
+public class UniformGenerator {
+    public static final String YES = "YES";
+    public static final String NO = "NO";
+
+    public  UniformGenerator() {
+    }
+
+    public static boolean check(int step, int mod) {
+        boolean isRight = false;
+        Set<Integer> set = new HashSet<>();
+
+        int x = 0;
+        set.add(x);
+
+        for (int i = 1; i < mod; i++) {
+            x = (x + step) % mod;
+            set.add(x);
+        }
+
+        return set.size() == mod ? true : false;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        PrintStream out = System.out;
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            int step = sc.nextInt();
+            int mod = sc.nextInt();
+
+            if (check(step, mod)) {
+                out.format("%10d%10d    %s%n%n", step, mod, "Good Choice");
+            } else {
+                out.format("%10d%10d    %s%n%n", step, mod, "Bad Choice");
+            }
+        }
+        out.close();
+    }
+
+    // -----------MyScanner class for faster input----------
+    public static class FastInputReader {
+        private byte[] buf = new byte[16384];
+        private int    curChar;
+        private int    numChars;
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = System.in.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public Integer[] nextIntegerArray(int n) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        private boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+    }
+
+
+}
+

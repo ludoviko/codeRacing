e82b6d7c54b96b592be811312d05147b868edac7
ludoviko <ludovikoazuaje@yahoo.com>
2020-01-25 00:49:17 +0100

    Cses test data.

---
+++ src/test/java/com/lam/coder/cses/MainTest.java
@@ -0,0 +1,33 @@
+package com.lam.coder.cses;
+
+
+import com.lam.coder.codeforces.Main;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+    Main main;
+
+    @Before
+    public void setUp() throws Exception {
+        main = new Main();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        main  = null;
+    }
+
+    @Test
+    public void test() {
+//        Assert.assertThat("Should be the same.", main, CoreMatchers.is("expected"));
+//		Assert.assertEquals("", main.);
+//		Assert.assertArrayEquals(new int[] {}, main.);
+//		Assert.assertArrayEquals(new String[] {}, main.);
+//        Assert.assertThat("string", CoreMatchers.instanceOf(String.class) );
+//        assertThat(3.3, IsCloseTo.closeTo(3.33, 0.1));
+
+    }
+}
+

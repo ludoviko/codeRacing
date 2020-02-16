b3ab490739d5461a7443aefc39fda4d8129070ed
ludoviko <ludovikoazuaje@yahoo.com>
2020-02-16 21:50:04 +0100

    Cses test.

--- src/main/java/com/lam/coder/codeforces/dataLoad/CopyClassFromTemplate.java
+++ src/main/java/com/lam/coder/codeforces/dataLoad/CopyClassFromTemplate.java
@@ -1,10 +1,6 @@
 package com.lam.coder.codeforces.dataLoad;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class CopyClassFromTemplate {

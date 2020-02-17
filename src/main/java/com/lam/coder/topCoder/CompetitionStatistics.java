6eb2ae5a10339682fd9af039c23174adb86e06d7
ludoviko <ludovikoazuaje@yahoo.com>
2020-02-17 23:20:59 +0100

    CompetitionStatistics	SRM 259.

---
+++ src/main/java/com/lam/coder/topCoder/CompetitionStatistics.java
@@ -0,0 +1,28 @@
+package com.lam.coder.topCoder;
+
+/**
+ * @author Code Forces.
+ *
+ * Solution by: L.Azuaje.
+ *         <p>
+ *Problem: CompetitionStatistics	SRM 259.
+ */
+public class CompetitionStatistics {
+
+    public int consecutiveGrowth(int[] ratingChanges) {
+        int maxSequence = 0;
+        int times = 0;
+
+        for (int rc: ratingChanges ) {
+            if (rc > 0) {
+                times++;
+                if (times > maxSequence) {
+                    maxSequence = times;
+                }
+            } else {
+                times = 0;
+            }
+        }
+        return maxSequence;
+    }
+}

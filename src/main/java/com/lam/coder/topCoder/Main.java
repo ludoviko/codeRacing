bde8e90482dfa194981737be81b89e57cb99e507
ludoviko <ludovikoazuaje@yahoo.com>
2020-02-20 02:02:30 +0100

    TopCoder main.

--- src/main/java/com/lam/coder/topCoder/Main.java
+++ src/main/java/com/lam/coder/topCoder/Main.java
@@ -1,16 +1,12 @@
 package com.lam.coder.topCoder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 
 /**
- * 
- * @author 
+ * @author TopCoder.
  *
- *
- *
+ * Solution by: L.Azuaje.
+ *         <p>
+ *Problem TopCoder: .
  */
 
 public class Main {
